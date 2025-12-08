f340fdb25292f095ceca035620da8b3ab8c02806
Nathanael <n.naveau@icloud.com>
2025-12-08 13:56:34 +0100

    Fix Next.js version, Node version and config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-  turbopack: {},
-
   images: {
     unoptimized: true,
   },
 
   serverExternalPackages: ["bufferutil", "utf-8-validate"],
-  
-    experimental: {
-    turbo: false
-  }
 };
 
 export default nextConfig;
